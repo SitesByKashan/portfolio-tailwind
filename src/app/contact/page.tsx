1375ca5d12d7bc26bf8c8363d56df836cc8bc04a
Kashan Malik <sitesbykashan@gmail.com>
2024-09-27 22:00:35 +0500

    .

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -59,7 +59,7 @@ export default function About() {
             height={1000}
             src={"/assets/Vector 3.png"}
             alt="Vector"
-            className="md:mt-96 mt-[1050px] w-full object-contain"
+            className="md:mt-96 mt-[1080px] w-full object-contain"
           />
         </div>
       </main>
