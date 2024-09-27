8e802ec6ca96adb80fc920e328abf4e8dcb3af0a
Kashan Malik <sitesbykashan@gmail.com>
2024-09-27 23:07:58 +0500

    .

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,11 +11,11 @@ export default function Navbar() {
                 <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
                     <Link href={'/'} className="mr-5 hover:text-[#398E8A] hover:bg-[#FFFFFF] p-2 rounded-3xl hover:px-6 hover:text-lg">Home</Link>
                     <Link href={'/about'} className="mr-5 hover:text-[#398E8A] hover:bg-[#FFFFFF] p-2 rounded-3xl hover:px-6 hover:text-lg">About</Link>
-                    <Link href={'/contact'} className="mr-5 hover:text-[#398E8A] hover:bg-[#FFFFFF] p-2 rounded-3xl hover:px-6 hover:text-lg">Contact Us</Link>
+                    <Link href={'/contact'} className="mr-5 hover:text-[#398E8A] hover:bg-[#FFFFFF] p-2 rounded-3xl hover:px-6 hover:text-lg">Contact</Link>
                 </nav>
                 <a href="/assets/KASHAN_MALIK_RESUME.pdf" download>
                     <button className="inline-flex items-center py-2 px-4 text-base mt-4 md:mt-0  hover:text-[#398E8A] hover:bg-[#FFFFFF] border border-white rounded-3xl">
-                        <span className="mr-2">Download CV</span>
+                        <span className="mr-2">Resume</span>
                         <FaDownload size={20} />
                     </button>
                 </a>
