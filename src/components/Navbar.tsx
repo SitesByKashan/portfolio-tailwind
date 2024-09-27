4a620f3012c7a42fb46bbb10c1ccc8e00e90c439
Kashan Malik <sitesbykashan@gmail.com>
2024-09-27 23:12:16 +0500

    .

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
         <header className="text-[#FFFFFF] body-font bg-[#398E8A]">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                 <Link href={'/'} className="flex title-font font-medium items-center  mb-4 md:mb-0">
-                    <span className="ml-3 text-4xl">M KASHAN MALIK AWAN</span>
+                    <span className="ml-3 text-2xl md:text-4xl">M KASHAN MALIK AWAN</span>
                 </Link>
                 <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
                     <Link href={'/'} className="mr-5 hover:text-[#398E8A] hover:bg-[#FFFFFF] p-2 rounded-3xl hover:px-6 hover:text-lg">Home</Link>
